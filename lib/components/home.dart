e90b8f7013b9221120622431f50f837503a47819
bilguun <mglgodbebe@gmail.com>
2025-12-01 15:14:04 +0800

    bebi

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,19 +1,69 @@
 import 'package:flutter/material.dart';
+import 'onboarding.dart';
+import 'login.dart';
+import 'register.dart';
+import 'order.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  // Pages shown for each tab
+  final List<Widget> pages = const [
+    Center(
+      child: Text(
+        'Welcome to the Home Page!',
+        style: TextStyle(fontSize: 24),
+      ),
+    ),
+    LoginPage(),
+    RegisterPage(),
+    OrderPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Delivery App'),
       ),
-      body: const Center(
-        child: Text(
-          'Welcome to the Home Page!',
-          style: TextStyle(fontSize: 24),
-        ),
+
+      body: pages[currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.orange ,
+        unselectedItemColor: Colors.black12,
+        showSelectedLabels: false,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Login',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.app_registration),
+            label: 'Register',
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Orders',
+          ),
+        ],
       ),
     );
   }
